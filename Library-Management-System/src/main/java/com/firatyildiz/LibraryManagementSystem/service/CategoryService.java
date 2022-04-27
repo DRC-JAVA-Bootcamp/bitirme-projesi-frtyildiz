3fc3e213864684b653cca19cf98c62d9b0cd96b6
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-04-27 17:01:47 +0300

    Update CategoryService.java

--- Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/service/CategoryService.java
+++ Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/service/CategoryService.java
@@ -1,9 +1,19 @@
 package com.firatyildiz.LibraryManagementSystem.service;
 
+import com.firatyildiz.LibraryManagementSystem.repository.CategoryRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryService {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
+
 
 
 }
