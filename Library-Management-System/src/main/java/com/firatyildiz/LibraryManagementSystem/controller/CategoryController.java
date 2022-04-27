99a6d144808217a7caefac469c4aa2a900e83917
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-04-27 17:12:14 +0300

    added findCategoryById

--- Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/controller/CategoryController.java
+++ Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/controller/CategoryController.java
@@ -1,11 +1,37 @@
 package com.firatyildiz.LibraryManagementSystem.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.firatyildiz.LibraryManagementSystem.dto.requestDto.SaveAuthorRequestDto;
+import com.firatyildiz.LibraryManagementSystem.dto.requestDto.SaveCategoryRequestDto;
+import com.firatyildiz.LibraryManagementSystem.entity.Author;
+import com.firatyildiz.LibraryManagementSystem.entity.Category;
+import com.firatyildiz.LibraryManagementSystem.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/author")
+@RequestMapping("/category")
 public class CategoryController {
 
+    @Autowired
+    CategoryService categoryService;
 
+    @PostMapping("/saveCategory")
+    public ResponseEntity<String> saveCategory(@RequestBody SaveCategoryRequestDto saveCategoryRequestDto)
+    {
+        String categorySaveDescription = categoryService.saveCategory(saveCategoryRequestDto);
+        return new ResponseEntity<>(categorySaveDescription, HttpStatus.OK);
+
+        /*
+            "categoryName":"Modern Klasikler",
+         */
+    }
+
+    @GetMapping("/findCategoryById")
+    public ResponseEntity<Category> findCategoryById(@RequestParam Integer categoryId)
+    {
+        Category category = categoryService.findCategory(categoryId);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
 }
--- Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/service/CategoryService.java
+++ Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/service/CategoryService.java
@@ -1,5 +1,8 @@
 package com.firatyildiz.LibraryManagementSystem.service;
 
+import com.firatyildiz.LibraryManagementSystem.dto.requestDto.SaveCategoryRequestDto;
+import com.firatyildiz.LibraryManagementSystem.entity.Author;
+import com.firatyildiz.LibraryManagementSystem.entity.Category;
 import com.firatyildiz.LibraryManagementSystem.repository.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,16 @@ public class CategoryService {
     @Autowired
     ModelMapper modelMapper;
 
+    public String saveCategory(SaveCategoryRequestDto saveCategoryRequestDto) {
+        Category category = modelMapper.map(saveCategoryRequestDto, Category.class);
 
+        category = categoryRepository.save(category);
 
+        return category.getCategoryName() + " Kategorisi Başarıyla Oluşturuldu.";
+    }
+
+    public Category findCategory(Integer categoryId)
+    {
+        return categoryRepository.findById(categoryId).get();
+    }
 }
