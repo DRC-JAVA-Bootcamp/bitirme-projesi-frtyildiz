5372bf7785f0dbbece273a752bb126688eaf90d6
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-05-05 02:14:36 +0300

    Added apache commons dependency

--- Library-Management-System/src/test/java/com/firatyildiz/LibraryManagementSystem/service/AuthorServiceTest.java
+++ Library-Management-System/src/test/java/com/firatyildiz/LibraryManagementSystem/service/AuthorServiceTest.java
@@ -1,10 +1,12 @@
 package com.firatyildiz.LibraryManagementSystem.service;
 
 import com.firatyildiz.LibraryManagementSystem.repository.AuthorRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
-
+@ExtendWith(MockitoExtension.class)
 public class AuthorServiceTest {
 
     @Mock
