77cba9f82cbd2cf38bf1a8700e530db0eb64e0b9
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-05-05 01:34:17 +0300

    Added junit dependency

---
+++ Library-Management-System/src/test/java/com/firatyildiz/LibraryManagementSystem/service/AuthorServiceTest.java
@@ -0,0 +1,22 @@
+package com.firatyildiz.LibraryManagementSystem.service;
+
+import com.firatyildiz.LibraryManagementSystem.repository.AuthorRepository;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+
+
+public class AuthorServiceTest {
+
+    @Mock
+    AuthorService authorService;
+
+    @Mock
+    AuthorRepository authorRepository;
+
+    @Mock
+    ModelMapper modelMapper;
+
+
+
+
+}
