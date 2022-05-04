f35987bd39655df962f3b15015212a436b8bdddf
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-05-05 02:36:20 +0300

    Added AuthorServiceValidation class

--- Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/validation/AuthorServiceValidation.java
+++ Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/validation/AuthorServiceValidation.java
@@ -5,24 +5,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthorServiceValidation {
 
-    public boolean istextual(String name)
+    public boolean istextual(String textualValue)
     {
-        if (org.apache.commons.lang3.StringUtils.isNumeric(name))
+        if (org.apache.commons.lang3.StringUtils.isNumeric(textualValue))
         {
             return false;
         }
-        if (org.apache.commons.lang3.StringUtils.isEmpty(name))
+        if (org.apache.commons.lang3.StringUtils.isEmpty(textualValue))
         {
             return false;
         }
-        if (name.trim().length() == 0)
+        if (textualValue.trim().length() == 0)
+        {
+            return false;
+        }
+        if (textualValue.length() > 50)
+        {
+            return false;
+        }
+        if (textualValue.startsWith(" ") || textualValue.startsWith(",") || textualValue.startsWith("-") || textualValue.startsWith("?") || textualValue.startsWith("=") || textualValue.startsWith("%") || textualValue.startsWith("+") || textualValue.startsWith("!") || textualValue.startsWith("'") || textualValue.startsWith("/") || textualValue.startsWith("<") || textualValue.startsWith(">"))
+        {
+            return false;
+        }
+        if (textualValue.endsWith(" ") || textualValue.endsWith(",") || textualValue.endsWith("-") || textualValue.endsWith("?") || textualValue.endsWith("=") || textualValue.endsWith("%") || textualValue.endsWith("+") || textualValue.endsWith("!") || textualValue.endsWith("'") || textualValue.endsWith("/") || textualValue.endsWith("<") || textualValue.endsWith(">"))
         {
             return false;
         }
 
         return true;
     }
-
 
 
 }
--- Library-Management-System/src/test/java/com/firatyildiz/LibraryManagementSystem/service/AuthorServiceTest.java
+++ Library-Management-System/src/test/java/com/firatyildiz/LibraryManagementSystem/service/AuthorServiceTest.java
@@ -1,13 +1,18 @@
 package com.firatyildiz.LibraryManagementSystem.service;
 
 import com.firatyildiz.LibraryManagementSystem.repository.AuthorRepository;
+import com.firatyildiz.LibraryManagementSystem.validation.AuthorServiceValidation;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthorServiceTest {
+
+    @InjectMocks
+    AuthorServiceValidation authorServiceValidation;
 
     @Mock
     AuthorService authorService;
