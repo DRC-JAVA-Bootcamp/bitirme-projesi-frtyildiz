2bdd020d082b975b818506e943c192f006320493
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-05-09 04:25:04 +0300

    Update AuthorResponseDto.java

--- Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/dto/responseDto/AuthorResponseDto.java
+++ Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/dto/responseDto/AuthorResponseDto.java
@@ -1,9 +1,6 @@
 package com.firatyildiz.LibraryManagementSystem.dto.responseDto;
 
-import com.firatyildiz.LibraryManagementSystem.entity.Book;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class AuthorResponseDto {
