892398722b701b863c4ca19fce130e0b1102d727
Fırat Yıldız <ksatriadecayeux@gmail.com>
2022-05-09 04:22:46 +0300

    Update BookController.java

--- Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/controller/BookController.java
+++ Library-Management-System/src/main/java/com/firatyildiz/LibraryManagementSystem/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.firatyildiz.LibraryManagementSystem.controller;
 
 import com.firatyildiz.LibraryManagementSystem.dto.requestDto.SaveBookRequestDto;
-import com.firatyildiz.LibraryManagementSystem.dto.requestDto.UpdateAuthorRequestDto;
 import com.firatyildiz.LibraryManagementSystem.dto.requestDto.UpdateBookRequestDto;
 import com.firatyildiz.LibraryManagementSystem.dto.responseDto.BookResponseDto;
 import com.firatyildiz.LibraryManagementSystem.entity.Book;
@@ -25,19 +24,6 @@ public class BookController {
     {
         String bookSaveDescription = bookService.saveBook(saveBookRequestDto);
         return new ResponseEntity<>(bookSaveDescription, HttpStatus.OK);
-
-        /*
-            "isbn":9789755393735,
-            "title":"Büyücü",
-            "authorId":2,
-            "publisher":"Ayrıntı Yayınları",
-            "language":"Türkçe",
-            "page":688,
-            "explanation":"Çağının yarı-entelektüel bunalımlarını geçirmekte olan, Oxford mezunu Nicholas Urfe, İngiltere’nin kasvetinden ve aşktan kaçmak için ücra bir Yunan adasına İngilizce öğretmeni olarak gider. Tek başına sıkıntılı günler geçirdiği, şair olduğuna dair hayallerinin de suya düştüğü bir sırada, gizemli milyoner Conchis ile tanışır...",
-            "publicationDate":"2021-11-11",
-            "format":"Ciltli",
-            "categoryId":1
-         */
     }
 
     @GetMapping("/findAllBook")
@@ -67,5 +53,4 @@ public class BookController {
         String deleteBookDescription = bookService.deleteBookById(bookId);
         return new ResponseEntity<>(deleteBookDescription, HttpStatus.OK);
     }
-
 }
